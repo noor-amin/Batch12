0fcd474caeedfd7cd9dace6d097ebe4fa7863a8f
nooramin <nooramin@bcd074612774>
2024-09-10 21:15:18 -0400

    push empty

--- src/homework/gitHomework.java
+++ src/homework/gitHomework.java
@@ -3,15 +3,4 @@
 public class gitHomework {
 	public static void main(String[] args) {
 		
-	}
-	public static void add (int a, int b) {
-		a = 12; 
-		b = 12;
-		//this is frim hub
-		System.out.println(a+b);
-	}
 	
-	public static void subtarct (int a , int b) {
-		System.out.println(a-b);
-	}
-}
