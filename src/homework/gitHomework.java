b5ca6f0c722b611ecdc3d1bf6d9fb26675777ec8
nooramin <nooramin@bcd074612774>
2024-09-10 20:43:13 -0400

    push 2

--- src/homework/gitHomework.java
+++ src/homework/gitHomework.java
@@ -7,4 +7,8 @@ public class gitHomework {
 	public static void add (int a, int b) {
 		System.out.println(a+b);
 	}
+	
+	public static void subtarct (int a , int b) {
+		System.out.println(a-b);
+	}
 }
